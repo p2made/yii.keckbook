80736d56ea2af5186fbdfd2a9f981015f17301d6
Pedro Plowman <pedrofp@me.com>
2016-03-27 19:37:40 +1000

    2016-26-27.5

--- requirements.php
+++ requirements.php
@@ -130,3 +130,4 @@ $requirements = array(
     ),
 );
 $requirementsChecker->checkYii()->check($requirements)->render();
+
--- yii.bat
+++ yii.bat
@@ -18,3 +18,4 @@ if "%PHP_COMMAND%" == "" set PHP_COMMAND=php.exe
 "%PHP_COMMAND%" "%YII_PATH%yii" %*
 
 @endlocal
+
