4effd80d173a7468c2c3dc691310efff80a8904a
Pedro Plowman <pedrofp@me.com>
2016-03-28 21:44:48 +1000

    2016-26-28.6

--- common/helpers/ValueHelpers.php
+++ common/helpers/ValueHelpers.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Gender.php
+ *
+ * @copyright Copyright &copy; Pedro Plowman, https://github.com/p2made, 2016
+ * @author Pedro Plowman
+ * @package p2made/yii.keckbook
+ * @license MIT
+ */
+
 namespace common\helpers;
 
 use yii;
@@ -13,68 +22,70 @@ class ValueHelpers
 
 	public static function roleMatch($name)
 	{
-		$userHasRoleName = Yii::$app->user->identity->role->name;
+		$userHasRoleName = Yii::$app->user->identity->role->role_name;
 		return $userHasRoleName == $name ? true : false;
 	}
 
 	public static function getUsersRoleValue($userId = null)
 	{
 		if ($userId == null){
-			$usersRoleValue = Yii::$app->user->identity->role->value;
+			$usersRoleValue = Yii::$app->user->identity->role->role_value;
 			return isset($usersRoleValue) ? $usersRoleValue : false;
 		}
 
 		$user = User::findOne($userId);
-		$usersRoleValue = $user->role->value;
+		$usersRoleValue = $user->role->role_value;
 		return isset($usersRoleValue) ? $usersRoleValue : false;
 	}
 
-	public static function getRoleValue($name)
+	public static function getRoleValue($role_name)
 	{
-		$role = Role::find('value')->where(['name' => $name])->one();
+		$role = Role::find('value')->where(['name' => $role_name])->one();
 		return isset($role->value) ? $role->value : false;
 	}
 
-	public static function isRoleNameValid($name)
+	public static function isRoleNameValid($role_name)
 	{
-		$role = Role::find('name')->where(['name' => $name])->one();
-		return isset($role->name) ? true : false;
+		$role = Role::find('role_name')->where(['role_name' => $role_name])->one();
+		return isset($role->role_name) ? true : false;
 	}
 
 	// ----- ^ ----- Status ----- ^ ----- //
 
-	public static function statusMatch($name)
+	public static function statusMatch($status_name)
 	{
-		$userHasStatusName = Yii::$app->user->identity->status->name;
-		return $userHasStatusName == $name ? true : false;
+		$userHasStatusName = Yii::$app->user->identity->status->status_name;
+		return $userHasStatusName == $status_name ? true : false;
 	}
 
-	public static function getStatusId($name)
+	public static function getStatusId($status_name)
 	{
-		$status = Status::find('id')->where(['name' => $name])->one();
+		$status = Status::find('id')->where(['status_name' => $status_name])->one();
 		return isset($status->id) ? $status->id : false;
 	}
 
 	// ----- ^ ----- UserType ----- ^ ----- //
 
-	public static function userTypeMatch($name)
+	public static function userTypeMatch($user_type_name)
 	{
-		$userHasUserTypeName = Yii::$app->user->identity->userType->name;
-		return $userHasUserTypeName == $name ? true : false;
+		$userHasUserTypeName = Yii::$app->user->identity->userType->user_type_name;
+		return $userHasUserTypeName == $user_type_name ? true : false;
 	}
 
 	// ----- ^ ----- ^ ----- ^ ----- ^ ----- //
-/*
-	public static function getStatusValue($name)
+
+	public static function getStatusValue($status_name)
 	{
-		$status = Status::find('value')->where(['name' => $name])->one();
-		return isset($status->value) ? $status->value : false;
+		$status = Status::find('status_value')->where(['status_name' => $status_name])->one();
+		return isset($status->status_value) ? $status->status_value : false;
 	}
 
-	public static function getUserTypeValue($name)
+	public static function getUserTypeValue($user_type_name)
 	{
-		$user_type = UserType::find('value')->where(['name' => $name])->one();
-		return isset($user_type->value) ? $user_type->value : false;
+		$user_type = UserType::find('user_type_value')->where([
+			'user_type_name' => $user_type_name
+		])->one();
+		return isset($user_type->user_type_value) ? $user_type->user_type_value : false;
 	}
-*/
+
 }
